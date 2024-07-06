29ef8a261ecd8bf74cedb86956c615b210cb495c
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-06 12:55:48 +0200

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,4 +8,14 @@ from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Takes a string `k` and a number `v` and returns a tuple with `k` as the first element and the square of `v` as the second element.
+
+    Args:
+        k (str): The string key.
+        v (Union[int, float]): The number value.
+
+    Returns:
+        Tuple[str, float]: A tuple with `k` as the first element and the square of `v` as the second element.
+    """
     return (k, v**2)
