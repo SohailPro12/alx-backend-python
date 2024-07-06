d91eae5959b1236f13aca8c458be598fc0e8410c
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-06 12:09:51 +0200

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
+    """
+    Adds two numbers and returns the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
     return a + b
