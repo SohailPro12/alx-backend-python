71e88ad61501489bbaf67c4a7eeda0fa1c55bf90
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-06 12:36:00 +0200

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+sum_list function
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    takes a list input_list of floats as argument and returns their sum as a float.
+    """
+    return sum(input_list)
