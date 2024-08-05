69d624925b62a46e22628d78d15530a799cc8f52
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-08-05 13:43:26 +0200

    task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,14 +9,16 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test access_nested_map function
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test(self, nested_map, path, expected_result):
-            result = access_nested_map(nested_map, path)
-            self.assertEqual(result, expected_result)
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
 
 if __name__ == '__main__':
