268110197ffdd1c005e381c580bc8181b1e915a9
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-08-05 14:46:16 +0200

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -59,13 +59,9 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """
-    Test memoize function
-    """
-    def test_memoize(self):
-        """
-        Test memoize function
-        """
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
         class TestClass:
             def a_method(self):
                 return 42
@@ -73,13 +69,15 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-
-        with patch.object(TestClass,
-                          'a_method', return_value=lambda: 42) as mock_method:
-            test = TestClass()
-            self.assertEqual(test.a_property, 42)
-            self.assertEqual(test.a_property, 42)
-            mock_method.assert_called_once()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
 
 
 if __name__ == '__main__':
